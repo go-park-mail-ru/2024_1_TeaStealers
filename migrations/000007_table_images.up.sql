628d7d88b076157c67b223e7298a5e1c42b6227d
ganger <malenko.2003@mail.ru>
2024-03-16 23:52:34 +0300

    migrate images

--- migrations/000007_table_images.up.sql
+++ migrations/000007_table_images.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS images (
-    id UUID NOT NULL PRIMARY KEY
-    /*advertId UUID NOT NULL REFERENCES adverts(id) ON DELETE SET NULL,
+    id UUID NOT NULL PRIMARY KEY,
+    advertId UUID NOT NULL REFERENCES adverts(id) ON DELETE SET NULL,
     photo TEXT CONSTRAINT photo_length CHECK ( char_length(photo) <= 255) NOT NULL,
     priority SMALLINT NOT NULL DEFAULT 1,
     dateCreation TIMESTAMP NOT NULL DEFAULT NOW(),
     isDeleted BOOLEAN NOT NULL DEFAULT FALSE,
-    UNIQUE(advertId, priority)*/
+    UNIQUE(advertId, priority)
 );
