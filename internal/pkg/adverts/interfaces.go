6ef970bc04067d11bdd2e9834aa0f7e2ac9ab848
ganger <malenko.2003@mail.ru>
2024-03-17 23:42:59 +0300

    transactions on create advert

--- internal/pkg/adverts/interfaces.go
+++ internal/pkg/adverts/interfaces.go
@@ -3,6 +3,7 @@ package adverts
 import (
 	"2024_1_TeaStealers/internal/models"
 	"context"
+	"database/sql"
 )
 
 // AdvertUsecase represents the usecase interface for adverts.
@@ -15,12 +16,13 @@ type AdvertUsecase interface {
 
 // AdvertRepo represents the repository interface for adverts.
 type AdvertRepo interface {
-	CreateAdvertType(ctx context.Context, newAdvertType *models.AdvertType) error
-	CreateAdvert(ctx context.Context, newAdvert *models.Advert) error
-	CreatePriceChange(ctx context.Context, newPriceChange *models.PriceChange) error
-	CreateBuilding(ctx context.Context, newBuilding *models.Building) error
-	CreateHouse(ctx context.Context, newHouse *models.House) error
-	CreateFlat(ctx context.Context, newFlat *models.Flat) error
+	BeginTx(ctx context.Context) (*sql.Tx, error)
+	CreateAdvertType(ctx context.Context, tx *sql.Tx, newAdvertType *models.AdvertType) error
+	CreateAdvert(ctx context.Context, tx *sql.Tx, newAdvert *models.Advert) error
+	CreatePriceChange(ctx context.Context, tx *sql.Tx, newPriceChange *models.PriceChange) error
+	CreateBuilding(ctx context.Context, tx *sql.Tx, newBuilding *models.Building) error
+	CreateHouse(ctx context.Context, tx *sql.Tx, newHouse *models.House) error
+	CreateFlat(ctx context.Context, tx *sql.Tx, newFlat *models.Flat) error
 	CheckExistsBuilding(ctx context.Context, adress string) (*models.Building, error)
 	GetHouseSquareAdvertsList(ctx context.Context) ([]*models.AdvertSquareData, error)
 	GetFlatSquareAdvertsList(ctx context.Context) ([]*models.AdvertSquareData, error)
