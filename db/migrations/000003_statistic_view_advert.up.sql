e60fe66d27b22f22b4e1cccce906ec27cf9446a0
ganger <malenko.2003@mail.ru>
2024-04-14 23:48:43 +0300

    fix static view

--- db/migrations/000003_statistic_view_advert.up.sql
+++ db/migrations/000003_statistic_view_advert.up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS statistic_view_advert (
     user_id BIGINT NOT NULL REFERENCES "user"(id),
     advert_id BIGINT NOT NULL REFERENCES advert(id),
-    date_creation TIMESTAMP NOT NULL DEFAULT NOW(),
     is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY (user_id, advert_id)
 );
